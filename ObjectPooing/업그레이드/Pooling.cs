f4e87cda9113106ec9c05c1e8efab1ea842fad50
parkjun-0521 <103255864+parkjun-0521@users.noreply.github.com>
2024-10-05 19:35:57 +0900

    Update Pooling.cs

--- ObjectPooing/업그레이드/Pooling.cs
+++ ObjectPooing/업그레이드/Pooling.cs
@@ -1,8 +1,3 @@
-using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Pooling : MonoBehaviourPun {
     public static Pooling instance;                     // 싱글톤 생성
 
@@ -39,18 +34,11 @@ public class Pooling : MonoBehaviourPun {
         // 아이템 생성 
         foreach (GameObject obj in pools[key]) {
             if (obj != null && !obj.activeSelf) {
-                // 오브젝트에 포톤 추가 
-                PhotonView objPhotonView = obj.GetComponent<PhotonView>();
-                if (objPhotonView == null) {
-                    objPhotonView = obj.AddComponent<PhotonView>();
-                }
                 // 생성된 오브젝트 반환
                 select = obj;
-                // 오브젝트 생성 동기화 
-                if (PhotonNetwork.IsConnected && PhotonNetwork.InRoom)
-                    photonView.RPC("ActivateObject", RpcTarget.All, objPhotonView.ViewID);
-                else
-                    select.gameObject.SetActive(true);
+                // 오브젝트 생성 
+                select.gameObject.SetActive(true);
+                    
                 break;
             }
         }
@@ -68,24 +56,12 @@ public class Pooling : MonoBehaviourPun {
             if (prefab == null) return null;
 
             // 새로운 아이템 프리팹 생성 
-            if (PhotonNetwork.IsConnected && PhotonNetwork.InRoom) {
-                select = PhotonNetwork.Instantiate(prefab.name, pos, Quaternion.identity);
-            }
-            else {  // 테스트용
-                select = Instantiate(prefab, Vector3.zero, Quaternion.identity);
-            }
+            select = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
             // 풀에 아이템 추가 
             pools[key].Add(select);
         }
 
         return select;
-    }
-
-    // 아이템 생성 동기화 
-    [PunRPC]
-    public void ActivateObject(int viewID) {
-        PhotonView targetView = PhotonView.Find(viewID);
-        if (targetView != null)
-            targetView.gameObject.SetActive(true);
     }
 }
